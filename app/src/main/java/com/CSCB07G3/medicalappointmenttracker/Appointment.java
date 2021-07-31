82be181eaacf687c3f0d0883ef29e95e2b5ff7bb
Nghia Le <trungnghia.le@mail.utoronto.ca>
2021-07-31 22:04:50 +0700

    Adding fields and methods to User class

---
+++ app/src/main/java/com/CSCB07G3/medicalappointmenttracker/Appointment.java
@@ -0,0 +1,7 @@
+package com.CSCB07G3.medicalappointmenttracker;
+
+public class Appointment {
+    public Appointment(){
+
+    }
+}
--- app/src/main/java/com/CSCB07G3/medicalappointmenttracker/User.java
+++ app/src/main/java/com/CSCB07G3/medicalappointmenttracker/User.java
@@ -3,4 +3,19 @@ package com.CSCB07G3.medicalappointmenttracker;
 import java.io.Serializable;
 
 public abstract class User implements Serializable {
+    public String name;
+    public String userId;
+    public String passWord;
+
+    public User(String name, String userId, String passWord){
+        this.name = name;
+        this.userId = userId;
+        this.passWord = passWord;
+    }
+
+    public Appointment getAppointment(){
+        return new Appointment();
+    }
+
+    public abstract User signUp();
 }
