8913de4189749bb05dd224d3b423606ba7bd452a
Nghia Le <trungnghia.le@mail.utoronto.ca>
2021-08-01 14:46:28 +0700

    Add a new login page

--- app/src/main/java/com/CSCB07G3/medicalappointmenttracker/Doctor.java
+++ app/src/main/java/com/CSCB07G3/medicalappointmenttracker/Doctor.java
@@ -2,22 +2,23 @@ package com.CSCB07G3.medicalappointmenttracker;
 
 public class Doctor extends User{
     // delte these fields after user class is completed
-    int id;
+    String id;
     String name;
     String password;
-
-    public Doctor(){
-
-    }
 
     @Override
     public String toString(){
           return "Doctor{"+ "id="+id+", name='"+name+"', password='"+password+'\''+'}';
     }
 
-    public Doctor(String name, int id, String password){
-        this.name=name;
-        this.id=id;
-        this.password=password;
+    public Doctor(String name, String id, String password){
+        super(name, id, password);
+        this.name = name;
+        this.id = id;
+        this.password = password;
+    }
+
+    public Doctor signUp(){
+        return new Doctor(this.name, this.id, this.password);
     }
 }
---
+++ app/src/main/java/com/CSCB07G3/medicalappointmenttracker/data/LoginDataSource.java
@@ -0,0 +1,29 @@
+package com.CSCB07G3.medicalappointmenttracker.data;
+
+import com.CSCB07G3.medicalappointmenttracker.data.model.LoggedInUser;
+
+import java.io.IOException;
+
+/**
+ * Class that handles authentication w/ login credentials and retrieves user information.
+ */
+public class LoginDataSource {
+
+    public Result<LoggedInUser> login(String username, String password) {
+
+        try {
+            // TODO: handle loggedInUser authentication
+            LoggedInUser fakeUser =
+                    new LoggedInUser(
+                            java.util.UUID.randomUUID().toString(),
+                            "Jane Doe");
+            return new Result.Success<>(fakeUser);
+        } catch (Exception e) {
+            return new Result.Error(new IOException("Error logging in", e));
+        }
+    }
+
+    public void logout() {
+        // TODO: revoke authentication
+    }
+}
