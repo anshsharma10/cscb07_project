73e398da102cc1a1c45788e80e8d46dfe438d1a7
shang8024 <jiale.shang@mail.utoronto.ca>
2021-08-05 11:45:34 -0400

    add new field "birthday" to Patient class, implement functions for birthday in RegisterActivity

--- app/src/main/java/com/CSCB07G3/medicalappointmenttracker/Model/Patient.java
+++ app/src/main/java/com/CSCB07G3/medicalappointmenttracker/Model/Patient.java
@@ -1,20 +1,25 @@
 package com.CSCB07G3.medicalappointmenttracker.Model;
 
+import java.util.Date;
+
 public class Patient extends User {
     String medinfo;
+    Date birthday;
 
     @Override
     public String toString(){
-        return "Patient{"+ "name='"+name+"', password='"+passWord+"', medinfo='"+medinfo+'\''+'}';
+        return "Patient{"+ "id='"+userId+"', name='"+name+"', password='"+passWord+"',gender='"+gender+"', date of birth="+birthday+", medinfo='"+medinfo+'\''+'}';
     }
 
     public Patient(){
         super();
         this.medinfo = "";
+        this.birthday = new Date();
     }
-    public Patient(String name, String userid,String password, String gender,String medinfo){
+    public Patient(String name, String userid, String password, String gender, String medinfo, Date birthday){
         super(name, userid, password, gender);
         this.medinfo = medinfo;
+        this.birthday = birthday;
     }
 
     public Patient signUp() {
@@ -34,6 +39,10 @@ public class Patient extends User {
     public void setMedInfo(String medInfo){
         this.medinfo = medInfo;
     }
-
     public String getMedInfo(){ return medinfo; }
+
+    public void setBirthday(Date birthday){
+        this.birthday = birthday;
+    }
+    public Date getBirthday(){ return birthday; }
 }
